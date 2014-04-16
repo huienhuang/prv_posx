9e56ab4da1a9c7020efd0fb3707167b2dd8d6232
pk <huienhuang@gmail.com>
2014-04-16 12:29:09 -0700

    report

--- request/reportgeneral.py
+++ request/reportgeneral.py
@@ -4,6 +4,8 @@ import time
 import config
 import re
 import datetime
+import cStringIO
+import csv
 
 
 PERM_ADMIN = 1 << config.USER_PERM_BIT['admin']
@@ -86,7 +88,33 @@ class RequestHandler(App.load('/advancehandler').RequestHandler):
             ]
         }
         return {'type':'chart', 'config': chart_config}
+    
+    def export_report_average_order_value(self, frm_ts, to_ts):
+        js = self.get_data_file_cached('receipt_report', 'receipt_report.txt')
+        if not js: return
         
+        lst = []
+        for k,v in js['summary']:
+            if frm_ts and to_ts and (k < frm_ts or k > to_ts): continue
+            if not v[0]: continue
+            lst.append( (k, round(v[1] / float(v[0]), 2), v[0]) )
+            
+        return (('AvgPerOrder', 'TotalOrdersCount'), lst)
+    
+    def export_report_ar_ages(self, frm_ts, to_ts):
+        js = self.get_data_file_cached('qb_ar_dues', 'qb_ar_dues.txt')
+        if not js: return
+        
+        lst = []
+        for k,v in js['dues']:
+            if frm_ts and to_ts and (k < frm_ts or k > to_ts): continue
+            if not v[-1]: continue
+            b = float(v[-1]) / 100
+            lst.append( (k, round(v[0] / b, 2), round(v[1] / b, 2), round(v[2] / b, 2), round(v[3] / b, 2)) )
+        
+        return (('<=30', '<=60', '<=90', '90+'), lst)
+
+
     def report_ar_ages(self, frm_ts, to_ts):
         js = self.get_data_file_cached('qb_ar_dues', 'qb_ar_dues.txt')
         if not js: return
@@ -155,8 +183,54 @@ class RequestHandler(App.load('/advancehandler').RequestHandler):
         }
         
         return {'type':'chart', 'config': chart_config}
+    
+    def export_report_active_customers(self, frm_ts, to_ts):
+        js = self.get_data_file_cached('customer_report', 'customer_report.txt')
+        if not js: return
+        
+        frm_tp = time.localtime(frm_ts)
+        to_tp = time.localtime(to_ts)
+        min_d = frm_tp.tm_year * 12 + frm_tp.tm_mon
+        max_d = to_tp.tm_year * 12 + to_tp.tm_mon
+        
+        lst = []
+        for i in range(min_d, max_d + 1): lst.append( [None, None, None, None] )
+        for k,v in js['retention_rate']:
+            if frm_ts and to_ts and (k < frm_ts or k > to_ts): continue
+            if not v[0]: continue
+            b = float(v[0]) / 100
+            r = round(v[1] / b, 2)
+            if r >= 1000: continue
+            
+            cur_tp = time.localtime(k)
+            l = lst[ cur_tp.tm_year * 12 + cur_tp.tm_mon - min_d ]
+            l[0] = k
+            l[1] = r
+            l[2] = round(v[2] / b, 2)
+        
+        for k,v in js['active_counts']:
+            if frm_ts and to_ts and (k < frm_ts or k > to_ts): continue
+            
+            cur_tp = time.localtime(k)
+            l = lst[ cur_tp.tm_year * 12 + cur_tp.tm_mon - min_d ]
+            l[0] = k
+            l[3] = v
+            
+        return (('New+Old', 'Old Only', 'ActiveCount'), lst)
         
         
+    def export_report_delivery_perfect_order(self, frm_ts, to_ts):
+        js = self.get_data_file_cached('delivery_report', 'delivery_report.txt')
+        if not js: return
+        
+        lst = []
+        for k,v in js['mons']:
+            if frm_ts and to_ts and (k < frm_ts or k > to_ts): continue
+            if not len(v['nums']): continue
+            lst.append( (k, round(v['perfect'] * 100 / float(len(v['nums'])), 2)) )
+        
+        return (('PerfectRate',), lst)
+    
     def report_delivery_perfect_order(self, frm_ts, to_ts):
         js = self.get_data_file_cached('delivery_report', 'delivery_report.txt')
         if not js: return
@@ -176,9 +250,20 @@ class RequestHandler(App.load('/advancehandler').RequestHandler):
                 {'toolTipContent': '{x}: {y}, Count: {count}', 'type': "line", 'xValueType': "dateTime", 'dataPoints': dps}
             ]
         }
-        return {'type':'chart', 'config': chart_config}
+        return {'type':'chart', 'config': chart_config} 
         
+    def export_report_worker_productivity(self, frm_ts, to_ts):
+        js = self.get_data_file_cached('delivery_report', 'delivery_report.txt')
+        if not js: return
         
+        lst = []
+        for k,v in js['mons']:
+            if frm_ts and to_ts and (k < frm_ts or k > to_ts): continue
+            if not v['work_secs']: continue
+            lst.append( (k, round(v['lines'] * 100 / (float(v['work_secs']) / 3600), 2)) )
+        
+        return (('ProductivityRate',), lst)  
+    
     def report_worker_productivity(self, frm_ts, to_ts):
         js = self.get_data_file_cached('delivery_report', 'delivery_report.txt')
         if not js: return
@@ -198,8 +283,7 @@ class RequestHandler(App.load('/advancehandler').RequestHandler):
                 {'toolTipContent': '{x}: {y}, Lines: {lines}', 'type': "line", 'xValueType': "dateTime", 'dataPoints': dps}
             ]
         }
-        return {'type':'chart', 'config': chart_config}   
-        
+        return {'type':'chart', 'config': chart_config}
         
     def report_inventory_to_sales_ratio(self, frm_ts, to_ts):
         js = self.get_data_file_cached('receipt_report', 'receipt_report.txt')
@@ -226,10 +310,65 @@ class RequestHandler(App.load('/advancehandler').RequestHandler):
         js = self.get_data_file_cached('receipt_report', 'receipt_report.txt')
         if not js: return
         
+        lst = []
         for k,v in js['summary']:
             if frm_ts and to_ts and (k < frm_ts or k > to_ts): continue
             if not v[3]: continue
-            dps.append( {'x': k * 1000, 'y': round(v[1] * 100 / float(v[3][0]), 2)} );
+            lst.append( (k, round(v[1] * 100 / float(v[3][0]), 2)) )
     
+        return (('Inventory/Sales',), lst)
     
+    def fn_export_reports(self):
+        js = self.req.psv_js('js')
+        
+        frm_ts = to_ts = 0
+        dt = js['frm_date']
+        if dt: frm_ts = int(time.mktime(time.strptime(dt, "%Y-%m")))
+        dt = js['to_date']
+        if dt: to_ts = int(time.mktime(time.strptime(dt, "%Y-%m")))
+        if not frm_ts or not to_ts: return
+        
+        frm_tp = time.localtime(frm_ts)
+        to_tp = time.localtime(to_ts)
+        min_d = frm_tp.tm_year * 12 + frm_tp.tm_mon
+        max_d = to_tp.tm_year * 12 + to_tp.tm_mon
+        len_d = max_d - min_d + 1
+        
+        hdr = ['', ]
+        for i in range(min_d, max_d + 1):
+            y,m = divmod(i, 12)
+            hdr.append('%04d-%02d' % (y, m))
+        
+        rows = [hdr, ]
+        for report in G_MAP_REPORTS:
+            if not(self.user_lvl & report[1]): continue
+            js = getattr(self, 'export_report_%s' % (report[2], ))(frm_ts, to_ts)
+            if not js: continue
+            
+            i_rows = []
+            hdr, lst = js
+            for h in range( len(hdr) ):
+                row = ['', ] * (len_d + 1)
+                row[0] = hdr[h]
+                i_rows.append(row)
+            
+            for l in lst:
+                if l[0] == None: continue
+                cur_tp = time.localtime(l[0])
+                idx = cur_tp.tm_year * 12 + cur_tp.tm_mon - min_d
+                
+                for i in range(1, len(l)):
+                    i_rows[i - 1][idx + 1] = (l[i] != None and str(l[i]) or '')
+            
+            i_rows.append( ['', ] * (len_d + 1) )
+            rows.extend(i_rows)
+        
+        fp = cStringIO.StringIO()
+        wt = csv.writer(fp)
+        for r in rows: wt.writerow(r)
+        
+        self.req.out_headers['content-type'] = 'application/octet-stream'
+        self.req.out_headers['content-disposition'] = 'attachment; filename="data.csv"'
+        self.req.write( fp.getvalue() )
+        
     
