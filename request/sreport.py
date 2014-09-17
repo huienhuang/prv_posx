f31251320327cb039791957cc77e5c97be53c0da
pk <huienhuang@gmail.com>
2014-09-17 13:12:53 -0700

    sreport

--- request/sreport.py
+++ request/sreport.py
@@ -8,6 +8,8 @@ import const
 import time
 import datetime
 import bisect
+import cStringIO
+import csv
 
 DEFAULT_PERM = 1 << config.USER_PERM_BIT['admin']
 class RequestHandler(App.load('/advancehandler').RequestHandler):
@@ -86,6 +88,63 @@ class RequestHandler(App.load('/advancehandler').RequestHandler):
         }
         self.req.writefile('cust_by_dept.html', r)
     
+
+
+    def get_cust_sales(self, frm, to):
+        frm_ts = to_ts = 0
+        if frm: frm_ts = int(time.mktime(time.strptime(frm, "%Y-%m")))
+        if to: to_ts = int(time.mktime(time.strptime(to, "%Y-%m")))
+        if frm_ts > to_ts: return []
+
+        cust_l = (self.get_data_file_cached('receipt_report', 'receipt_report.txt') or {}).get('customer', {}).items()
+        cust_l.sort(key=lambda f_x:f_x[0])
+
+        n_cust_l = []
+        for f_cid,f_dt_l in cust_l:
+            ldt_k = [ f_x[0] for f_x in f_dt_l ]
+            ttl_sales = 0
+            for dt in f_dt_l[bisect.bisect_left(ldt_k, frm_ts):bisect.bisect_right(ldt_k, to_ts)]:
+                ttl_sales += dt[1][0]
+            if ttl_sales: n_cust_l.append( (f_cid, ttl_sales) )
+
+        return n_cust_l
+
+    def fn_export_cust_sales(self):
+        js = self.req.psv_js('js')
+        n_cust_l = self.get_cust_sales(js.get('frm'), js.get('to'))
+        if not n_cust_l: return
+
+        cust_d = {}
+        cur = self.cur()
+        cur.execute('select sid,name,detail from sync_receipts_customers where sid in (%s)' % ( ','.join([str(f_x[0]) for f_x in n_cust_l]) ))
+        for r in cur.fetchall(): cust_d[ r[0] ] = r
+
+        data = [ ('Name', 'Address', 'Phone', 'Sales') ]
+        for cid,sales in n_cust_l:
+            sid,name,detail = cust_d.get(cid)
+            gjs = json.loads(detail)
+            addr = []
+            addr.append(gjs['addr1'] + (gjs['addr2'] and ' ' + gjs['addr2'] or ''))
+            addr.append(gjs['city'])
+            addr.append(gjs['state'] + (gjs['zip'] and ' ' + gjs['zip'] or ''))
+            phone = gjs['phone']
+
+            r = (
+                name,
+                ','.join([f_a for f_a in addr if f_a]),
+                phone,
+                sales and '%0.2f' % (sales, ) or '',
+            )
+            data.append(r)
+
+        fp = cStringIO.StringIO()
+        wt = csv.writer(fp)
+        for r in data: wt.writerow(r)
+        
+        self.req.out_headers['content-type'] = 'application/octet-stream'
+        self.req.out_headers['content-disposition'] = 'attachment; filename="data.csv"'
+        self.req.write( fp.getvalue() )
+
     def fn_get(self):
         ret = {'res':{'len':0, 'apg':[]}}
 
@@ -94,57 +153,37 @@ class RequestHandler(App.load('/advancehandler').RequestHandler):
         eidx = self.qsv_int('eidx')
         if pgsz > 100 or eidx - sidx > 5: self.req.exitjs(ret)
         
-        frm_ts = to_ts = 0
-        dt = self.qsv_str('frm')
-        if dt: frm_ts = int(time.mktime(time.strptime(dt, "%Y-%m")))
-        dt = self.qsv_str('to')
-        if dt: to_ts = int(time.mktime(time.strptime(dt, "%Y-%m")))
-        if frm_ts > to_ts: self.req.exitjs(ret)
-        
-        rjs = (self.get_data_file_cached('receipt_report', 'receipt_report.txt') or {}).get('customer', {})
-        
+        n_cust_l = self.get_cust_sales(self.qsv_str('frm'), self.qsv_str('to'))
+        rlen = len(n_cust_l)
+
         cur = self.cur()
         apg = []
         if pgsz > 0 and sidx >= 0 and sidx < eidx:
-            cur.execute('select SQL_CALC_FOUND_ROWS sid,name,detail from sync_customers order by sid desc limit %d,%d' % (
-                sidx * pgsz, (eidx - sidx) * pgsz
-                )
-            )
-            nzs = cur.column_names
-            for r in cur:
-                sid,name,detail = r
+            n_cust_l = n_cust_l[sidx * pgsz : sidx * pgsz + (eidx - sidx) * pgsz]
+            if n_cust_l:
+                cust_d = {}
+                cur.execute('select sid,name,detail from sync_receipts_customers where sid in (%s)' % ( ','.join([str(f_x[0]) for f_x in n_cust_l]) ))
+                for r in cur.fetchall(): cust_d[ r[0] ] = r
+
+            for cid,sales in n_cust_l:
+                sid,name,detail = cust_d.get(cid)
                 gjs = json.loads(detail)
                 addr = []
-                addr.append(gjs['address1'] + (gjs['address2'] and ' ' + gjs['address2'] or ''))
+                addr.append(gjs['addr1'] + (gjs['addr2'] and ' ' + gjs['addr2'] or ''))
                 addr.append(gjs['city'])
                 addr.append(gjs['state'] + (gjs['zip'] and ' ' + gjs['zip'] or ''))
-                phone = gjs['phone1']
-                
-                ldt = rjs.get(sid, [])
-                ldt_k = [ f_x[0] for f_x in ldt ]
-                ttl_sales = 0
-                for dt in ldt[bisect.bisect_left(ldt_k, frm_ts):bisect.bisect_right(ldt_k, to_ts)]:
-                    ttl_sales += dt[1][0]
-                    
+                phone = gjs['phone']
+
                 r = (
                     name,
                     ','.join([f_a for f_a in addr if f_a]),
                     phone,
-                    ttl_sales and '%0.2f' % (ttl_sales, ) or '',
+                    sales and '%0.2f' % (sales, ) or '',
                     str(sid)
                 )
                 apg.append(r)
-        
-            cur.execute('select FOUND_ROWS()')
-            
-        else:
-            cur.execute('select count(*) from sync_customers')
-            
-        rlen = int(cur.fetchall()[0][0])
+
         res = ret['res']
         res['len'] = rlen
         res['apg'] = apg
         self.req.writejs(ret)
-        
-        
-        
