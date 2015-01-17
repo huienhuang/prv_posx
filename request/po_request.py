f729b38c139f8a64795b2e6fcd73d62a18792fcc
pkt <huienhuang@gmail.com>
2015-01-16 17:49:06 -0800

    request

--- request/po_request.py
+++ request/po_request.py
@@ -6,4 +6,18 @@ DEFAULT_PERM = 1 << config.USER_PERM_BIT['purchasing_mgr']
 class RequestHandler(App.load('/advancehandler').RequestHandler):
 
     def fn_default(self):
-        self.req.writefile("po_request.html")
+        tabs = [('PO', 'PO'), ('View', 'VIEW')]
+        r = {
+            'tab_cur_idx' : 3,
+            'title': 'PO REQUEST',
+            'tabs': tabs
+        }
+        self.req.writefile('tmpl_multitabs.html', r)
+
+
+    def fn_po(self):
+        self.req.writefile('purchasing/po_list.html')
+        
+    def fn_view(self):
+        self.req.writefile('purchasing/po_request.html')
+    
