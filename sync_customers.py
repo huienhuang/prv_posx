8412090ec8164712d074148fa99ae002e666afb8
pkt <huienhuang@gmail.com>
2015-02-23 17:31:22 -0800

    sync

--- sync_customers.py
+++ sync_customers.py
@@ -3,7 +3,12 @@ import sys
 import json
 import time
 import data_helper
+import cPickle
 
+SNAPSHOT_CFNS = [u'Type', u'Title', u'FName', u'LName', u'Company', u'Address1', u'Address2', u'City', u'State', u'ZIP', u'Country', u'ShipAddrName', u'ShipCompany', u'ShipFullName', u'ShipAddress', u'ShipAddress2', u'ShipCity', u'ShipState', u'ShipZIP', u'ShipCountry', u'Phone1', u'Phone2', u'Phone3', u'Phone4', u'Email', u'Comments', u'UDF1', u'UDF2', u'UDF3', u'UDF4', u'UDF5', u'UDF6', u'UDF7', u'TaxArea', u'PriceLevel', u'DiscType', u'TrackRewards', u'CustomerID', u'WebNumber', u'WebFullNumber', u'EmailNotify', u'AR', u'UseAccCharge', u'AcceptChecks', u'DefaultShipTo', u'NoShipToBill', u'CreditLimit', u'DiscAllowed']
+
+CFNS_FLOAT = "disctype,creditused".split(',')
+CFNS_STR = "discallowed,type,lname,company,address1,address2,city,state,zip,country,phone1,phone2,phone3,phone4,email,udf1,udf2,udf3,udf5,taxarea,customerid".split(',')
 
 QB_VFS = (
     ('state', None, True),
@@ -22,14 +27,67 @@ def check_inconsistency(r):
     
     return len(incon)
 
+
+def create_snapshots(mdb, lrs, mode):
+    if mode:
+        reps = lrs
+        while reps:
+            qjs = []
+            for r in reps[:1000]: qjs.append( "(%d,%d,'%s')" % (r[0], r[1], mdb.escape_string(cPickle.dumps(r[2], 1))) )
+            mdb.query("insert into sync_customer_snapshots values " + ','.join(qjs))
+            reps = reps[1000:]
+        return
+
+
+    mdb.query('select * from sync_customer_snapshots where sid in (%s)' % ( ','.join(str(f_x[0]) for f_x in lrs) ))
+    mdb.use_result()
+    rs = {}
+    for r in mdb.fetch_row(maxrow=0): rs[ r[0] ] = cPickle.loads(r[2])
+
+    reps = []
+    difs = []
+    for sid,lts,n in lrs:
+        o = rs.get(sid)
+        if o == None:
+            reps.append( (sid, lts, n) )
+            continue
+        else:
+            dif = []
+            for i in range(len(SNAPSHOT_CFNS)):
+                if o[i] != n[i]: dif.append( (i, n[i]) )
+            if dif:
+                reps.append( (sid, lts, n) )
+                difs.append( (sid, lts, dif) )
+
+    reps_len = len(reps)
+    difs_len = len(difs)
+
+    while reps:
+        qjs = []
+        for r in reps[:1000]: qjs.append( "(%d,%d,'%s')" % (r[0], r[1], mdb.escape_string(cPickle.dumps(r[2], 1))) )
+        mdb.query("insert into sync_customer_snapshots values " + ','.join(qjs))
+        reps = reps[1000:]
+
+    while difs:
+        qjs = []
+        for r in difs[:1000]: qjs.append( "(%d,%d,'%s')" % (r[0], r[1], mdb.escape_string(cPickle.dumps(r[2], 1))) )
+        mdb.query("insert into sync_customer_chg values " + ','.join(qjs))
+        difs = difs[1000:]
+
+
+    if reps_len or difs_len: print 'CHG: S(%d), C(%d)' % (reps_len, difs_len)
+
+
 def sync_customers(cj_data, mode=0):
     cur,sur = pos_db()
     mdb = sys_db()
     
-    feed = []
+    lrs = []
     
     if mode:
         mdb.query('delete from sync_customers')
+        mdb.query('delete from sync_customer_snapshots')
+        mdb.query('delete from sync_customer_chg')
         where_sql = 'datastate=0'
     else:
         sids = [ str(x[1]) for x in cj_data if x[0] and x[0].lower() == 'customer' ]
@@ -38,21 +96,29 @@ def sync_customers(cj_data, mode=0):
 
     rep_seq = del_seq = 0
     sql = ''
-    cur.execute("select sid,datastate,pricelevel,disctype,creditused,discallowed,type,lname,company,address1,address2,city,state,zip,country,phone1,phone2,phone3,phone4,email,udf1,udf2,udf3,udf5,taxarea,customerid from customer where " + where_sql)
+    cur.execute("select * from customer where " + where_sql)
     cur_nzs = [ d[0].lower() for d in cur.description ]
     for r in cur.rows():
-        r = list(r[:3]) + map(rf2, r[3:5]) + [ x or '' for x in r[5:] ]
         r = dict(zip(cur_nzs, r))
-        
+
         if r['datastate']:
             del_seq += 1
             mdb.query("delete from sync_customers where sid=%d limit 1" % (r['sid'],))
             feed.append( (r['sid'], 1) )
             continue
+
+        lr = []
+        for f_k in SNAPSHOT_CFNS: lrs.append(r[f_k])
+        
+        for f_k in CFNS_FLOAT: r[f_k] = rf2(r[f_k])
+        for f_k in CFNS_STR: r[f_k] = r[f_k] or ''
         
         sid = r['sid']
         del r['sid']
         feed.append( (sid, 0) )
+
+        lts = int(time.mktime(time.strptime(r['lastedit'].split('.')[0], '%Y-%m-%d %H:%M:%S')))
+        lrs.append( (sid, lts, lr) )
         
         #generate location hash
         r['loc'] = data_helper.get_location_hash(r['address1'], r['city'], r['state'], r['zip'])
@@ -92,22 +158,19 @@ def sync_customers(cj_data, mode=0):
         mdb.query(sql)
         sql = ''
     
+    create_snapshots(mdb, lrs, mode)
+
     while feed:
         qjs = mdb.escape_string( json.dumps(feed[:1000], separators=(',',':')) )
-        mdb.query("insert into sync_feed values (null,1,'%s')" % (
-            qjs,
-            )
-        )
+        #mdb.query("insert into sync_feed values (null,1,'%s')" % (
+        #    qjs,
+        #    )
+        #)
         mdb.query("insert into sync_chg values (null,1,'%s')" % (
             qjs,
             )
         )
         feed = feed[1000:]
-    
-    #if rep_seq or del_seq:
-    #    if mode:
-    #        mdb.query('optimize table sync_customers')
-    #        mdb.use_result()
     
     print 'sync_customers: R(%d), D(%d)' % (rep_seq, del_seq)
     return rep_seq + del_seq
@@ -118,4 +181,3 @@ g_sync_cb = ('customers', sync_customers)
 if __name__ == '__main__':
     sync_main(g_sync_cb)
     #sync_customers(None, 1)
-    #sync_customers( (('customer', -7777123806535057151), ('customer', -7777123797550857983), ('customer', -7777123806493114111)) )
